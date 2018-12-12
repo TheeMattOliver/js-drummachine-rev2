5dc2cd3bab95aefd56ac327cd1ae853723e78d3e
Matt Oliver <mattoliver.mattoliver@gmail.com>
2018-12-12 11:31:35 -0600

    moving js to own file

---
+++ js/drum.js
@@ -0,0 +1,19 @@
+function playSound(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"`);
+    console.log("Audio: ", audio);
+
+    if(!audio) return //stop the function from running all together   
+    audio.currentTime=0; //rewind to the start    
+    audio.play();
+    key.classList.add('playing'); 
+}
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return; // skip it if it's not a transform
+  this.classList.remove('playing');
+  }
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound) 
